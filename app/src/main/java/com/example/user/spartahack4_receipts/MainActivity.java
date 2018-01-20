b1b2ee335aba5c9135110019f8a23a0e280aea70
Blake <blakey626@yahoo.com>
2018-01-20 16:47:03 -0500

    Branch commit test

--- app/src/main/java/com/example/user/spartahack4_receipts/MainActivity.java
+++ app/src/main/java/com/example/user/spartahack4_receipts/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.user.spartahack4_receipts;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//I AM BLAKE'S
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
